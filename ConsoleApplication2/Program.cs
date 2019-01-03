b7ed366815d383b089f5b0104e607a908c1d4134
Kirin9586 <tuanho02k11@viendong.edu.vn>
2019-01-03 17:32:56 +0700

    Cack

---
+++ ConsoleApplication2/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n, i = 0,sum=0;
+            Console.Write("Input n:");
+            n = int.Parse(Console.ReadLine());
+            for (i = 0; i <= n; i++)
+                sum = sum + i;
+            Console.Write("Tong {0}\n", sum);
+            Console.ReadKey();
+        }
+    }
+}
